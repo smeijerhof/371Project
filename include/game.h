57b6ff4b71175d4fab4f5716d836e01e9279042f
Angela Yung <angela_yung@sfu.ca>
2023-07-29 10:11:39 -0700

    Add client id

--- include/game.h
+++ include/game.h
@@ -101,6 +101,7 @@ struct Game {
     int actorNum = 0;
     Actor actors[4] {};
     int totalActors = 0;
+    int clientID;
 
     // New method to update game state based on client's mouse positions
     void updateGameState(int clientID, int mouseX, int mouseY) {
