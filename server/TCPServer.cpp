64ced03437c4bb22fc6c5cf0a0854df848ca12bd
Sebastian Meijerhof <smeijerh@sfu.ca>
2023-07-31 15:32:27 -0700

    testing: added bare bones of Angela's server multi-threading to branch

--- server/TCPServer.cpp
+++ server/TCPServer.cpp
@@ -1,5 +1,66 @@
 #include "../include/server.h"
 
+// Function to handle client connections in separate threads
+void* clientHandler(void* arg) {
+    int clientSocket = *((int*)arg);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create the game instance
+    Game game;
+    game.start();
+
+    // Send the client's ID to the client
+    unsigned char clientID = game.connectClient();
+    unsigned char idBuffer[2] = { 'I', clientID };
+    write(clientSocket, idBuffer, sizeof(idBuffer));
+
+    // Calculate the size of the message buffer based on the number of fish positions
+    size_t bufferSize = 1 + FISH_NUM * 4;
+    unsigned char* msgBuffer = new unsigned char[bufferSize];
+    msgBuffer[0] = 0; // Points
+
+    while (true) {
+        // Receive data from the client (mouse positions in serialized form)
+        int bytesRead = read(clientSocket, buffer, sizeof(buffer));
+        if (bytesRead <= 0) {
+            // Client disconnected or error occurred
+            close(clientSocket);
+            game.disconnectClient(clientID);
+            break;
+        }
+
+        // Extract mouse positions (mouseX = buffer[2], mouseY = buffer[3])
+        int mouseX = buffer[2];
+        int mouseY = buffer[3];
+
+        // Update the game state using the extracted mouse positions
+        mtx.lock();
+        game.updateGameState(clientID, mouseX, mouseY);
+        mtx.unlock();
+
+        // Fill the message buffer with the game fish positions
+        mtx.lock();
+        for (int i = 0; i < FISH_NUM; i++) {
+            int x = (int)game.positions[i].x;
+            int y = (int)game.positions[i].y;
+
+            msgBuffer[(i * 4) + 1] = (x >> 8) & 0xFF;
+            msgBuffer[(i * 4) + 2] = x & 0xFF;
+            msgBuffer[(i * 4) + 3] = (y >> 8) & 0xFF;
+            msgBuffer[(i * 4) + 4] = y & 0xFF;
+        }
+        mtx.unlock();
+
+        // Send the message containing the game fish positions to the client
+        write(clientSocket, msgBuffer, bufferSize);
+    }
+
+    // Clean up the dynamically allocated buffer
+    delete[] msgBuffer;
+
+    return NULL;
+}
+
 int main(int argc, char const *argv[]) {
     time_t seed { 0 };
     seed = time(NULL);
@@ -15,10 +76,18 @@ int main(int argc, char const *argv[]) {
     if(l < 0) 
         printError("Listen failed");
 
-    // wait until connection established, then read message
-    int connectionSocket = accept(server.sock, 0, 0);
-    if(connectionSocket < 0)
-        printError("Failed to accept\n");
+    std::vector<std::thread> clientThreads;
+
+    // Accept and handle client connections in separate threads
+    while ((clientSocket = accept(serverSocket, (struct sockaddr*)&address, (socklen_t*)&addrlen))) {
+        if(clientSocket < 0)
+            printError("Failed to accept\n");
+        // Create a new thread to handle the client
+        std::thread clientThread(clientHandler, (void*)&clientSocket);
+
+        // Detach the thread to allow it to run independently
+        clientThread.detach();
+    }
 
     while(1) {
         // read data from connection, convert to appropriate endian
