f0c7e88529b3a7b0570e89e8d138210b3809a9d7
Artinan <julian.mjm.300@gmail.com>
2023-08-04 18:31:16 -0700

    Update def.h

--- include/def.h
+++ include/def.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#define SERVER_PORT 65500
+#define SERVER_PORT 65502
 #define IP "localhost"
 #define BUFFER_SIZE 32
-#define QUEUE_SIZE 10
+#define QUEUE_SIZE 4
+#define PLACEHOLDER 1000
 
 #define FISH_NUM 10
+#define PLAYER_NUM 4
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 450
