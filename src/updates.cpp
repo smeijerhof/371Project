9344fb5ca73965baa596224c5808c982fad67105
Artinan <julian.mjm.300@gmail.com>
2023-08-06 16:18:06 -0700

    Added comments

--- src/updates.cpp
+++ src/updates.cpp
@@ -1,9 +1,11 @@
+// client_updates.h has comments explaining the uses of all the functions defined in this file
 #include "../include/client_updates.h"
 
 void* sendUpdateMessage(void* msg) {
 	UpdateMessage* message = (UpdateMessage*) msg;
 	message->clearResponse();
-	
+
+	// Update messages send the message token (present in all messages), as well as the client number, mouse x and y positions, and score 
 	message->setResponse(message->token);
 	message->setResponse(message->clientNumber);
 	message->setResponse(message->mouseX);
@@ -20,7 +22,8 @@ void* sendUpdateMessage(void* msg) {
 		return nullptr;
 	}
 	
-	// Handle other player data
+	// We handle all other player's data to update visual information
+	// ...clients[i].lure is a variable keeping track of which fish the player is currently luring. This isnt sent in the update message, but it is received
 	for (int i = 0; i < message->state->numberOfClients; i++) {
 		message->state->clients[i].pos.x = (float) message->readResponse();
 		message->state->clients[i].pos.y = (float) message->readResponse();
@@ -28,7 +31,7 @@ void* sendUpdateMessage(void* msg) {
 		message->state->clients[i].lure = message->readResponse();
 	}
 	
-	// Handle dead fishes
+	// The server also sends whether or not any fish has died from other users.
 	for (int i = 0; i < FISH_NUM; i++) {
 		uint16_t alive = message->readResponse();
 		if (alive == 0) message->state->fishes[i].alive = false;
@@ -42,7 +45,8 @@ void* sendCatchMessage(void* msg) {
 	
 	FishMessage* message = (FishMessage*) msg;
 	message->clearResponse();
-	
+
+	// Update messages send the message token (present in all messages), as well as the client number, and the fish being lured 
 	message->setResponse(message->token);
 	message->setResponse(message->clientNumber);
 	message->setResponse(message->fishIndex);
@@ -56,7 +60,8 @@ void* sendCatchMessage(void* msg) {
 		printf("Error in reading server response\n");
 		return nullptr;
 	}
-	
+
+	// The value of success is either positive or negative, and is the servers response as to whether or not luring the fish was successful
 	uint16_t success = message->readResponse();
 	
 	printf("success = %d\n", success);
@@ -67,11 +72,13 @@ void* sendCatchMessage(void* msg) {
 		return 0;
 	} else if (success == MESSAGE_POSITIVE) {
 		printf("	-> Success in luring fish!\n");
+		// Update the client to be catching the fish
 		message->state->catching = true;
 		message->state->fishes[message->fishIndex].taken = message->clientNumber;
 		message->state->fishLife = 10;
 		message->state->target = message->fishIndex;
 	} else {
+		// Should only ever happen upon a corrupted message
 		printf("	-> corrupted response from the server");
 	}
 	
@@ -81,7 +88,8 @@ void* sendCatchMessage(void* msg) {
 void* sendTerminateMessage(void* msg) {
 	FishMessage* message = (FishMessage*) msg;
 	message->clearResponse();
-	
+
+	// Update messages send the message token (present in all messages), as well as the client number, the fish being lured, and whether or not the user succeeded in catching it
 	message->setResponse(message->token);
 	message->setResponse(message->clientNumber);
 	message->setResponse(message->fishIndex);
@@ -98,7 +106,8 @@ void* sendTerminateMessage(void* msg) {
 void* sendOverMessage(void* msg) {
 	ClientMessage* message = (ClientMessage*) msg;
 	message->clearResponse();
-	
+
+	// Update messages send the message token. This message is simply to request the winner from the server
 	message->setResponse(message->token);
 	
 	write(message->serverSocket, message->response, 2 * BUFFER_SIZE);
@@ -112,7 +121,8 @@ void* sendOverMessage(void* msg) {
 		printf("Error in reading server response\n");
 		return nullptr;
 	}
-	
+
+	// Update which player won the game
 	message->state->winner = message->readResponse();
 	printf("Player %d won the game\n", message->state->winner);
 	
